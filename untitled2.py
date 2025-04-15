13d66a2392915d834bf3b5ec59cd208167ace001
xandasoneill <xandas.oneill@gmail.com>
2025-04-15 13:56:16 +0100

    Add files via upload

---
+++ untitled2.py
@@ -0,0 +1,15 @@
+
+from graphics import *
+ 
+class Table():
+     
+    def oval(p1, p2, window):
+             Oval(Point(*p1), Point(*p2)).draw(window)
+         
+    def rectangular(p1, p2, window):
+        Rectangle(Point(*p1), Point(*p2)).draw(window)
+    
+    def circular(p1, radius, window):
+        Circle(Point(*p1), radius).draw(window)
+        
+        #fwewgwegwrh
